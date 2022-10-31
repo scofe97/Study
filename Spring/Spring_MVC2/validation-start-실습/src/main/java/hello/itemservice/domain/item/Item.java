fe8c902b0a6d75159edfe8a780843453fd58abca
tscofet <tscofet@gmail.com>
2022-11-01 06:35:34 +0900

    2022/11/01

--- Spring/Spring_MVC2/validation-start-실습/src/main/java/hello/itemservice/domain/item/Item.java
+++ Spring/Spring_MVC2/validation-start-실습/src/main/java/hello/itemservice/domain/item/Item.java
@@ -2,23 +2,29 @@ package hello.itemservice.domain.item;
 
 import lombok.Data;
 import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
+
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Data
+// @ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "10000원 넘게 입력해라")
 public class Item {
 
+    @NotNull(groups = UpdateCheck.class) // 수정 요구사항
     private Long id;
-    @NotBlank
+
+    @NotBlank(groups = {UpdateCheck.class, SaveCheck.class})
     private String itemName;
-    @NotNull
-    @Range(min = 1000, max = 10000)
+
+    @NotNull(groups = {UpdateCheck.class, SaveCheck.class})
+    @Range(min = 1000, max = 10000, groups = {UpdateCheck.class, SaveCheck.class})
     private Integer price;
 
-    @Max(9999)
-    @NotNull
+    @NotNull(groups = {UpdateCheck.class, SaveCheck.class})
+    @Max(value = 9999, groups = {SaveCheck.class} )
     private Integer quantity;
 
     public Item() {
