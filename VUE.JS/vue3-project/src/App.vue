d31b92036f5d624229a47d29e548542569acf33f
tscofet <tscofet@gmail.com>
2022-11-07 20:47:22 +0900

    2022/11/07

---
+++ VUE.JS/vue3-project/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="name">
+    {{ name }}
+  </div>
+  <div>
+    {{ greeting() }}
+  </div>
+  <div>
+    {{ greeting2('Bohyeon') }}
+  </div>
+  <div>
+    {{ greeting2(name) }}
+  </div>
+  <div>
+    {{ greeting3 }}
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    const name = "Kossie Coder";
+
+    // 함수사용
+    const greeting = () => {
+      return "Hello"
+    }
+
+    // 함수사용2
+    const greeting2 = (name) => {
+      return "Hello " + name
+    }
+
+    const greeting3 = greeting2(name);
+
+    return {
+      name,
+      greeting,
+      greeting2,
+      greeting3,
+    };
+  },
+};
+</script>
+
+<style>
+.name {
+  color: red;
+}
+</style>
