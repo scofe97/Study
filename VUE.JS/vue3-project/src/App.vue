e2351c8626f1ef553fcb0a071fd982d6cc4fbbe5
bohyeon <tscofet@gamil.com>
2022-11-10 17:56:13 +0900

    2022/11/10

--- VUE.JS/vue3-project/src/App.vue
+++ VUE.JS/vue3-project/src/App.vue
@@ -1,21 +1,24 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <router-link class="navbar-brand" :to="{ name: 'Home' }">Kossie Coder</router-link>
+    <router-link class="navbar-brand" :to="{ name: 'Home' }"
+      >Kossie Coder</router-link
+    >
 
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <router-link class="nav-link" :to="{ name: 'Todos' }">Todos <span class="sr-only">(current)</span></router-link>
+        <router-link class="nav-link" :to="{ name: 'Todos' }"
+          >Todos <span class="sr-only">(current)</span></router-link
+        >
       </li>
     </ul>
   </nav>
-  <router-view />
+  <div class="container">
+    <router-view />
+  </div>
 </template>
 
 <script>
-export default {
-}
+export default {};
 </script>
 
-<style>
-
-</style>
+<style></style>
--- VUE.JS/vue3-project/src/components/TodoList.vue
+++ VUE.JS/vue3-project/src/components/TodoList.vue
@@ -1,66 +1,64 @@
 <template>
-    <div v-for="(todo, index,) in todos" :key="todo.id" class="card mt-2">
-        <div class="card-body p-2 d-flex align-items-center"
-        @click="moveToPage(todo.id)">
-            <div class="form-check flex-grow-1">
-                <input class="form-check-input" type="checkbox" :checked="todo.completed" 
-                style="pointer"
-                @change="toggleTodo(index, $evnet)" @click.stop>
-                <label class="form-check-label" :class="{ todo: todo.completed }">
-                    {{ todo.subject }}
-                </label>
-            </div>
-            <div>
-                <button class="btn btn-danger btn-sm" @click.stop="deleteTodo(index)">Delete</button>
-            </div>
-
-        </div>
+  <div v-for="(todo, index) in todos" :key="todo.id" class="card mt-2">
+    <div
+      class="card-body p-2 d-flex align-items-center"
+      style="cursor: pointer"
+      @click="moveToPage(todo.id)">
+      <div class="form-check flex-grow-1">
+        <input
+          class="form-check-input"
+          type="checkbox"
+          :checked="todo.completed"
+          @change="toggleTodo(index, $event)"
+          @click.stop />
+        <label class="form-check-label" :class="{ todo: todo.completed }">
+          {{ todo.subject }}
+        </label>
+      </div>
+      <div>
+        <button class="btn btn-danger btn-sm" @click.stop="deleteTodo(index)">
+          Delete
+        </button>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-import { useRouter  } from 'vue-router'
-
+import { useRouter } from "vue-router";
 export default {
-    props: {
-        todos: {
-            type: Array,
-            required: true
-        }
+  props: {
+    todos: {
+      type: Array,
+      required: true,
     },
-    emits: ['toggle-todo', 'delete-todo'],
-    setup(props, context) {
-        const router = useRouter();
+  },
+  emits: ["toggle-todo", "delete-todo"],
+  setup(props, { emit }) {
+    const router = useRouter();
+    const toggleTodo = (index, event) => {
+      emit("toggle-todo", index, event.target.checked);
+    };
 
-        const toggleTodo = (index. event) => {
-            context.emit('toggle-todo', index, event.target.checked);
-        };
-
-        const deleteTodo = (index) => {
-            context.emit('delete-todo', index);
-        };
-
-        const moveToPage = (todoId) => {
-            console.log(todoId);
-            router.push({
-                name: 'Todo',
-                params: {
-                    id: todoId
-                }
-            });
-        }
-
-        return {
-            toggleTodo,
-            deleteTodo,
-            moveToPage
-        }
-    }
-
-
-}
+    const deleteTodo = (index) => {
+      emit("delete-todo", index);
+    };
+    const moveToPage = (todoId) => {
+      // router.push('/todos/' + todoId);
+      router.push({
+        name: "Todo",
+        params: {
+          id: todoId,
+        },
+      });
+    };
+    return {
+      toggleTodo,
+      deleteTodo,
+      moveToPage,
+    };
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
