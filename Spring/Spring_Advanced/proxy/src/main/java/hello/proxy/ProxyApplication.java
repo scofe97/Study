40420ffe76606edf4935e6282dfe2af8e72887b9
BaHoBuNi <cloud0314@naver.com>
2023-06-22 19:14:59 +0900

    2023/06/22

--- Spring/Spring_Advanced/proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ Spring/Spring_Advanced/proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -1,9 +1,6 @@
 package hello.proxy;
 
-import hello.proxy.config.AppV1Config;
-import hello.proxy.config.AppV2Config;
-import hello.proxy.config.v1_proxy.ConcreteProxyConfig;
-import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
+import hello.proxy.config.v2_dynamicproxy.DynamicProxyFilterConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -11,10 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
-import java.util.ArrayList;
-
 // @Import({AppV1Config.class, AppV2Config.class})
-@Import(ConcreteProxyConfig.class)
+@Import(DynamicProxyFilterConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 
